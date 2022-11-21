9cf04f5f8fdabefa4e72606fccc3bb84fd240041
gihanrcg <gihanrcg1997@gmail.com>
2022-11-21 23:12:43 +0530

    new changes

--- src/models/EditTaskModel.js
+++ src/models/EditTaskModel.js
@@ -35,30 +35,31 @@ const EditTaskModel = ({modal, toggle, updateTask, taskObj}) => {
     }
 
     return (
-        <Modal isOpen={modal} toggle={toggle}>
-            <ModalHeader toggle={toggle}>Update Task</ModalHeader>
-            <ModalBody>
-                <form>
-                    <div className='form-group'>
-                        <label >Task Name</label>
-                        <input type='text' className='form-control' name='taskName' value={taskName} onChange = {handleChange}/>
-                    </div>
-                    <div className='form-group pt-3'>
-                        <label>Description</label>
-                        <textarea rows='5' className='form-control' name='description' value={description} onChange = {handleChange}></textarea>
-                    </div>
+        // <Modal isOpen={modal} toggle={toggle}>
+        //     <ModalHeader toggle={toggle}>Update Task</ModalHeader>
+        //     <ModalBody>
+        //         <form>
+        //             <div className='form-group'>
+        //                 <label >Task Name</label>
+        //                 <input type='text' className='form-control' name='taskName' value={taskName} onChange = {handleChange}/>
+        //             </div>
+        //             <div className='form-group pt-3'>
+        //                 <label>Description</label>
+        //                 <textarea rows='5' className='form-control' name='description' value={description} onChange = {handleChange}></textarea>
+        //             </div>
                     
-                </form>
-            </ModalBody>
-            <ModalFooter>
-                <Button color="primary" type='submit' onClick={handleUpdate}>
-                    Update
-                </Button>{' '}
-                <Button color="secondary" onClick={toggle}>
-                    Cancel
-                </Button>
-            </ModalFooter>
-        </Modal>
+        //         </form>
+        //     </ModalBody>
+        //     <ModalFooter>
+        //         <Button color="primary" type='submit' onClick={handleUpdate}>
+        //             Update
+        //         </Button>{' '}
+        //         <Button color="secondary" onClick={toggle}>
+        //             Cancel
+        //         </Button>
+        //     </ModalFooter>
+        // </Modal>
+        <div></div>
     );
 
     
